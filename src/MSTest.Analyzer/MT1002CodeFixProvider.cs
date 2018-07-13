5481385df01e25d80ec88a72451632eb22f4a824
superfaz <fkmels-github@yahoo.fr>
2018-07-13 22:14:01 +0200

    Implement MT1002

---
+++ src/MSTest.Analyzer/MT1002CodeFixProvider.cs
@@ -0,0 +1,86 @@
+// Copyright (c) Francois Karman. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MSTest.Analyzer
+{
+    /// <summary>
+    /// Provide code fix for the <see cref="MT1002Analyzer"/> analyzer.
+    /// </summary>
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MT1002CodeFixProvider))]
+    [Shared]
+    public class MT1002CodeFixProvider : CodeFixProvider
+    {
+        /// <inheritdoc />
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(MT1002Analyzer.DiagnosticId); }
+        }
+
+        /// <summary>
+        /// Returns the code fixer that can be used to fix multiple instance of the MT1002 issue.
+        /// </summary>
+        /// <returns>The code fixer.</returns>
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        /// <inheritdoc />
+        public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            string title = "Remove [TestClass] attribute";
+
+            foreach (Diagnostic diagnostic in context.Diagnostics)
+            {
+                CodeAction action = CodeAction.Create(
+                    title,
+                    cancellationToken => RemoveTestClassAttributeAsync(context.Document, diagnostic, cancellationToken),
+                    nameof(MT1002CodeFixProvider));
+                context.RegisterCodeFix(action, diagnostic);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static async Task<Document> RemoveTestClassAttributeAsync(Document document, Diagnostic diagnostic, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+
+            // Remove the [TestClass] attribute
+            var node = root.FindNode(diagnostic.Location.SourceSpan)
+                .AncestorsAndSelf()
+                .OfType<ClassDeclarationSyntax>()
+                .First();
+
+            List<SyntaxNode> nodesToRemove = new List<SyntaxNode>();
+            foreach (var list in node.AttributeLists)
+            {
+                if (list.Attributes.Count == 1
+                    && list.Attributes.First().Name.ToString() == "TestClass")
+                {
+                    nodesToRemove.Add(list);
+                }
+                else if (list.Attributes.Count > 1)
+                {
+                    nodesToRemove.AddRange(list.Attributes.Where(a => a.Name.ToString() == "TestClass"));
+                }
+            }
+
+            root = root.RemoveNodes(nodesToRemove, SyntaxRemoveOptions.KeepNoTrivia);
+
+            return document.WithSyntaxRoot(root);
+        }
+    }
+}
---
+++ test/MSTest.Analyzer.Tests/MT1002UnitTest.cs
@@ -0,0 +1,112 @@
+// Copyright (c) Francois Karman. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest.Analyzer.Helpers;
+using System;
+
+namespace MSTest.Analyzer
+{
+    /// <summary>
+    /// Tests the <see cref="MT1002Analyzer"/> analyzer
+    /// as well as the associated <see cref="MT1002CodeFixProvider"/> code fixer.
+    /// </summary>
+    [TestClass]
+    public class MT1002UnitTest : CodeFixVerifier
+    {
+        /// <summary>
+        /// Tests with an empty code.
+        /// </summary>
+        [TestMethod]
+        public void EmptyCode()
+        {
+            var test = string.Empty;
+
+            this.VerifyCSharpDiagnostic(test);
+        }
+
+        /// <summary>
+        /// Tests with a valid structure.
+        /// </summary>
+        [TestMethod]
+        public void ValidCode()
+        {
+            var test = @"
+using System;
+
+namespace TestPackage
+{
+    internal class TypeName
+    {   
+    }
+}";
+
+            this.VerifyCSharpDiagnostic(test);
+        }
+
+        /// <summary>
+        /// Tests with an invalid structure.
+        /// </summary>
+        [TestMethod]
+        public void InvalidCode()
+        {
+            var test = @"
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestPackage
+{
+    [TestClass]
+    internal class TypeName
+    {
+    }
+}";
+            var expected = new DiagnosticResult
+            {
+                Id = "MT1002",
+                Message = "The class 'TypeName' should not be marked with the [TestClass] attribute",
+                Severity = DiagnosticSeverity.Warning,
+                Location = new DiagnosticResultLocation("Test0.cs", 8, 20)
+            };
+
+            this.VerifyCSharpDiagnostic(test, expected);
+
+            var fixtest = @"
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestPackage
+{
+    internal class TypeName
+    {
+    }
+}";
+            this.VerifyCSharpFix(test, fixtest, allowNewCompilerDiagnostics: true);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the CSharp diagnostic analyzer begin tested.
+        /// </summary>
+        /// <returns>
+        /// A new instance of <see cref="MT1002Analyzer"/>.
+        /// </returns>
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new MT1002Analyzer();
+        }
+
+        /// <summary>
+        /// Creates a new instance of the CSharp code fix begin tested.
+        /// </summary>
+        /// <returns>
+        /// A new instance of <see cref="MT1002CodeFixProvider"/>.
+        /// </returns>
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new MT1002CodeFixProvider();
+        }
+    }
+}
